1d6c9a0420f16ff103ef31e45bfcfaf72ab4f8f3
minxf1 <minh10t2cht@gmail.com>
2026-01-28 00:44:05 +0700

    Uprade the patch to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.ayn_thor_bottom_screen_power_tool"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
