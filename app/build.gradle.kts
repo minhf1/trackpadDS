5b85582ead376b2b2290b16f56b944732b3f2d42
minxf1 <minh10t2cht@gmail.com>
2026-02-18 22:05:08 +0700

    V1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.ayn_thor_bottom_screen_power_tool"
         minSdk = 33
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
